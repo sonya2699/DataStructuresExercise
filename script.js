d155650a651eb293f1fc1cbc45242e236b407e7d
Student <student@Cumberlandss-MacBook-Pro.local>
2021-09-30 00:42:06 -0500

    Completed the DataStructuresExercise with parital Bonus

---
+++ script.js
@@ -0,0 +1,87 @@
+// Data Structures Exercise
+
+// #1a
+const jedi = [];
+console.log(jedi);
+
+// #1b
+jedi [0] = "Luke";
+console.log(jedi);
+
+// #1c
+jedi.push("Obi-Wan Kenobi");
+console.log(jedi);
+
+// #1d
+jedi.unshift("Yoda");
+console.log(jedi);
+
+// #1e
+console.log(jedi[1]);
+
+// #1f
+jedi.pop();
+console.log(jedi);
+
+// #1g
+jedi.shift();
+console.log(jedi);
+
+// #2a
+const sithLords = ["Darth Vader", "Darth Sidious","Darth Maul"];
+console.log(sithLords);
+
+// #2b
+const imperialOfficers = ["Grand Moff Tarkin", "Orson Krennic"];
+console.log(imperialOfficers);
+
+// #2c
+const starWarsVillians = sithLords.concat(imperialOfficers);
+console.log(starWarsVillians);
+
+// #2d
+console.log(starWarsVillians.slice(0,2));
+
+// #3a
+const droids = {
+astromech: "R2-D2", 
+protocol: "C-3PO", 
+assassin: "IG-88"
+}
+console.log(droids);
+
+// #3b
+console.log("R2-D2");
+
+// #3c
+console.log("C-3PO");
+
+// #3d
+(droids["assassin"]) = "IG-11";
+console.log(droids);
+
+// Bonus
+// #4
+console.log(starWarsVillians[0][6]);
+
+// #5
+
+
+// #6a
+const starWarsMovies = [
+{ episodeOne: "The Phantom Menace", 
+episodeTwo: "Attack of the Clones", episodeThree: "Revenge of the Sith"
+},
+{
+episodeFour: "A New Hope", 
+episodeFive: "The Empire Strikes Back", episodeSix: "Return of the Jedi"
+},
+{
+episodeSeven: "The Force Awakens", 
+episodeEight: "The Last Jedi", 
+episodeNine: "The Rise of Skywalker"
+}];
+console.log(starWarsMovies);
+
+// #6b
+
